27c23182ae3d52cd304b7fef865164c239f148df
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2018-09-03 07:28:13 +0300

    adding POJOs. Closes #2.

---
+++ src/main/java/name/nicholasgribanov/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package name.nicholasgribanov.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/name/nicholasgribanov/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package name.nicholasgribanov.sfgpetclinic.model;
+
+public class Vet extends Person {
+}

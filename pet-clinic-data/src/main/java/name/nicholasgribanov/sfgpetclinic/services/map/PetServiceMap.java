c0851b775e44b5d7ce1f0f95987fae17d03d1a95
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2018-09-06 07:49:21 +0300

    Impl Map based Services. closes #6

---
+++ pet-clinic-data/src/main/java/name/nicholasgribanov/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package name.nicholasgribanov.sfgpetclinic.services.map;
+
+import name.nicholasgribanov.sfgpetclinic.model.Pet;
+import name.nicholasgribanov.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/name/nicholasgribanov/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package name.nicholasgribanov.sfgpetclinic.services.map;
+
+import name.nicholasgribanov.sfgpetclinic.model.Vet;
+import name.nicholasgribanov.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}

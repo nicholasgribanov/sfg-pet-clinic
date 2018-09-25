bffdbade26a91ec487ff48e820e862e7557fd61b
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2018-09-25 07:56:31 +0300

    added PetTypeService. closes #29

---
+++ pet-clinic-data/src/main/java/name/nicholasgribanov/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package name.nicholasgribanov.sfgpetclinic.services;
+
+import name.nicholasgribanov.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}

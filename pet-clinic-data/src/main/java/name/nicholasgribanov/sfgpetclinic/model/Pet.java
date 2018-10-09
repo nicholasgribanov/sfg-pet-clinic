4fb8c68a9ca6f0986e651a85efda3abe98174323
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2018-10-09 07:56:15 +0300

    Updated owner with JPA entity mappings. closes #36

--- pet-clinic-data/src/main/java/name/nicholasgribanov/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/name/nicholasgribanov/sfgpetclinic/model/Pet.java
@@ -1,14 +1,26 @@
 package name.nicholasgribanov.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
-    private PetType petType;
-    private Owner owner;
-    private LocalDate birthDay;
+    @Column(name = "name")
     private String name;
 
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    @Column(name = "birth_day")
+    private LocalDate birthDay;
 
     public PetType getPetType() {
         return petType;

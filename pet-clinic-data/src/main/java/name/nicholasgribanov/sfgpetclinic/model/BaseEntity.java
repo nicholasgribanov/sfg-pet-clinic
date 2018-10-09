dabb7263e8f7cfdeed0d7e54c2e4120a3852af45
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2018-10-09 07:45:28 +0300

    Updated base entity with JPA Annotations. closes #35

--- pet-clinic-data/src/main/java/name/nicholasgribanov/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/name/nicholasgribanov/sfgpetclinic/model/BaseEntity.java
@@ -1,7 +1,17 @@
 package name.nicholasgribanov.sfgpetclinic.model;
 
-public class BaseEntity {
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
 
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
